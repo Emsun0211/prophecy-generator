7765a16d3d670321857c1b6991c5e529afdd6094
shadrach akaade <akaadesm@gmail.com>
2022-11-25 13:15:22 +0100

    implement footer component

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,15 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import Footer from "./component/Footer";
 import Header from "./component/Navbar";
 import Home from "./pages/Home/home";
-
 
 const RouteSwitch = () => {
   return (
     <>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   );
 };
--- src/component/Navbar/index.jsx
+++ src/component/Navbar/index.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 //import { Link } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
-	return (
-	<Navbar bg="light" expand="lg">
+  return (
+    <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">Prophecy Generator</Navbar.Brand>
+        <Navbar.Brand href="/" className="text-blue-500 navlink">
+          Prophecy Generator
+        </Navbar.Brand>
         <Navbar.Toggle className="border-none" aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="ms-auto my-2 text-sky-500 text-center my-lg-0"
-            style={{ maxHeight: '100px' }}
+            className="ms-auto my-2 text-blue-500 text-center my-lg-0"
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Prophecies</Nav.Link>
-            <Nav.Link href="#action2">About</Nav.Link>
-            <Nav.Link href="#action2">Contact</Nav.Link>
+            <Nav.Link href="/" className="text-blue-500 navlink">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#action2" className="text-blue-500 navlink">
+              Prophecies
+            </Nav.Link>
+            <Nav.Link href="#action2" className="text-blue-500 navlink">
+              About
+            </Nav.Link>
+            <Nav.Link href="#action2" className="text-blue-500 navlink">
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-)}
+  );
+};
 
 export default Header;
