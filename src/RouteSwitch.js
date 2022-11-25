ad996e51d1ca2b3d6cb28a239e7b35e741e3dba5
shadrach akaade <akaadesm@gmail.com>
2022-11-25 03:48:04 +0100

    setup routes and implement header

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,2 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./component/Navbar";
+import Home from "./pages/Home/home";
 
-rf
+
+const RouteSwitch = () => {
+  return (
+    <>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </>
+  );
+};
+
+export default RouteSwitch;
--- src/component/Navbar/index.jsx
+++ src/component/Navbar/index.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+//import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-export const index = () => {
-	return <div>index</div>;
-};
+const Header = () => {
+	return (
+	<Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Prophecy Generator</Navbar.Brand>
+        <Navbar.Toggle className="border-none" aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="ms-auto my-2 text-sky-500 text-center my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Prophecies</Nav.Link>
+            <Nav.Link href="#action2">About</Nav.Link>
+            <Nav.Link href="#action2">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+)}
+
+export default Header;
